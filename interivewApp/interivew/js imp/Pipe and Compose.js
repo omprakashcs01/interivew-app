1723a8f1e76bb284c42dcc0f9bd9cdb059f79db6
Om Prakash <omprakashcs01@gmail.com>
2024-06-21 02:00:25 +0530

    ad

--- interivewApp/interivew/js imp/Pipe and Compose.js
+++ interivewApp/interivew/js imp/Pipe and Compose.js
@@ -16,3 +16,40 @@ const pipe =
   (...fns) =>
   val =>
     fns.reduce((prev, fn) => fn(prev), val);
+////////////////////////////////////////////////////////////////
+
+const addFive = num => {
+  return num + 5;
+};
+
+const subOne = num => {
+  return num - 1;
+};
+
+const multiplyTwo = num => {
+  return num * 2;
+};
+
+const evolute = compose(addFive, subOne, multiplyTwo);
+
+console.log(evolute(5));
+
+function compose(...args) {
+  return function (init) {
+    let result = init;
+    for (let i = args.length - 1; i >= 0; i--) {
+      result = args[i](result);
+    }
+    return result;
+  };
+}
+
+///inbuild
+
+function compose(...args) {
+  return function (init) {
+    return args.reduceRight((acc, curr) => {
+      return curr(acc);
+    }, init);
+  };
+}
