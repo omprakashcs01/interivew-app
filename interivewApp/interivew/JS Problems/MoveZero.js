2ced9f645cbff61b09590d625f4a8b96ad35ac29
Om Prakash <omprakashcs01@gmail.com>
2024-07-15 00:32:43 +0530

    add

--- interivewApp/interivew/JS Problems/MoveZero.js
+++ interivewApp/interivew/JS Problems/MoveZero.js
@@ -1,16 +1,54 @@
 function moveZero(arr) {
-    let left = 0;
-    let right = 0;
-  
-    while (right < arr.length) {
-      if (arr[right] !== 0) {
-        [arr[left], arr[right]] = [arr[right], arr[left]];
-        left++;
-      }
-      right++;
+  let left = 0;
+  let right = 0;
+
+  while (right < arr.length) {
+    if (arr[right] !== 0) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
     }
-    return arr
+    right++;
   }
-  
-  console.log(moveZero([0,1,0,3,12]));
-  
+  return arr;
+}
+
+console.log(moveZero([0, 1, 0, 3, 12]));
+
+function moveNegatives(arr) {
+  let left = 0;
+  let right = 0;
+
+  while (right < arr.length) {
+    if (arr[right] >= 0) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+    }
+    right++;
+  }
+  return arr;
+}
+
+// Example usage:
+let array = [0, -1, 2, -3, 4, -5];
+console.log(moveNegatives(array)); // Output: [0, 2, 4, -3, -1, -5]
+
+/////
+function moveNegativesToLeft(arr) {
+  let left = 0;
+  let right = 0;
+
+  while (right < arr.length) {
+    if (arr[right] < 0) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+    }
+    right++;
+  }
+  return arr;
+}
+
+// Example usage:
+let array = [0, -1, 2, -3, 4, -5];
+console.log(moveNegativesToLeft(array)); // Output: [-1, -3, -5, 2, 4, 0]
+
+
