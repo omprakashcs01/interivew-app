07e0ab00f0e8786cba3a6741907883fb1dae0d3b
@oprakash <Om.Prakash@dataart.com>
2026-08-05 13:44:51 +0530

    adding memoize

---
+++ interivewApp/interivew/js imp/memoization.js
@@ -0,0 +1,13 @@
+const memoize = (func) => {
+  const results = {};
+
+  return (...args) => {
+    const argsKey = JSON.stringify(args);
+
+    if (!results[argsKey]) {
+      results[argsKey] = func(...args);
+    }
+
+    return results[argsKey];
+  };
+};
